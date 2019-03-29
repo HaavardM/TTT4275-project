9e5a112c69f389ccbff6e35c43c4788137c4bee7
Olav Bech Bråten <olavbb@hotmail.com>
2019-03-29 09:00:46 +0100

    Clean up script

--- estimateMLE.m
+++ estimateMLE.m
@@ -9,49 +9,68 @@ T = 1/Fs;
 
 N = 513;
 M = 2^10;
-SNR_db = -10;
+SNR_db = 10;
 noise_std = sqrt(A^2/10^(SNR_db/10));
 
 steps = 10^5;
 omega_0_est = zeros(steps, 1);
 angle_est = zeros(steps,1);
 
-func = @(w, p) sum(abs(x - A*exp(1i*(w*[1:N]*T + p))));
 signal = A*exp(1i*(omega_0*[1:N]*T + ph));
 for j=1:steps
     noise = normrnd(0, noise_std, 1, N) + 1i*normrnd(0, noise_std, 1, N);
     x = signal + noise;
 
-    x_fft = fft(x, M)/M;
+    x_fft = fft(x,M)/M;
+
 
     [peak, pos] = max(x_fft);
-    omega_est = 2*pi*(pos)/(M*T);
+
+    omega_0_est(j) = 2*pi*(pos)/(M*T);
+    
     % TODO: The signs of these two components must be corrected
-    ang_est = angle(exp(1i*omega_0_est(j)*best_n0(N)*T)*peak');
-    
-    % We might want to do the estimate, THEN do the fminsearch? It takes a
-    % *long* time
-    [vals, ~, exitflag, output] = fminsearch(@(input) func(input(1), input(2)), [omega_est, ang_est]);
-    omega_0_est(j) = vals(1);
-    angle_est(j) = vals(2);
-    %omega_0_est(j) = 2*pi*(pos)/(M*T);
-    %angle_est(j) = angle(exp(1i*omega_0_est(j)*best_n0(N)*T)*-peak);
-    
-    
-    
-    if mod(j, steps/100) == 0
+    angle_est(j) = angle(exp(1i*omega_0_est(j)*best_n0(N)*T)*-peak);
+
+    if mod(j, steps/100) == 0 && false
         fprintf('%i%%\n',100* j/steps)
     end
 end
 clc;
-% angle_est = angle(exp(1i*omega_0_est*best_n0())
-err = mean(omega_0 - omega_0_est);
+%% Analyze
 
-omega_0_var = var(omega_0_est - omega_0);
-fprintf('Estimated angle: %f pi (%.3f%% off)\nError variance: %f | CRLB: %f\n\n', mean(angle_est)/pi,100*(mean(angle_est) - ph)/ph, var(angle_est - ph), best_phase(noise_std, A, N))
-fprintf('Estimated omega_0: %.0f (%.3f%% off)\nError variance: %f | CRLB: %f\n', mean(omega_0_est), 100*(mean(omega_0_est) - omega_0)/omega_0, omega_0_var, best_freq(noise_std, A, T, N));
+omega_err = (omega_0 - omega_0_est);
+omega_0_var = var(omega_err);
+phase_err = (ph - angle_est);
+phase_var = var(phase_err);
 
 
+%% Fminify
+% Minimize error of our estimate
+func = @(w, p) sum(abs(x - A*exp(1i*(w*[1:N]*T + p))));
+% Start with the estimate from the ffts
+[vals, ~, exitflag, output] = fminsearch(@(input) func(input(1), input(2)), [mean(omega_0_est), mean(angle_est)]);
+
+omega_mle = vals(1);
+phase_mle = vals(2);
+
+%% Printing
+fprintf('---- Input Data ----\n');
+fprintf('A:\t%f\tomega:\t%fHz\tPhase offset:\t%fpi\n', A, omega_0, ph/pi);
+fprintf('Fs:\t%fHz\tSNR:\t%idB\n', Fs, SNR_db);
+fprintf('N: %i\t FFT-Size: %i\t#Simulations: %i\n\n', N, M, steps);
+
+fprintf('---- Estimated Values (FFT-estimate) ----')
+fprintf('Estimated omega_0: %.0f (%.3f%% off)\n', mean(omega_0_est), 100*(mean(omega_0_est) - omega_0)/omega_0);
+fprintf('Error variance: %f | CRLB: %f\n\n', omega_0_var, best_freq(noise_std, A, T, N))
+fprintf('Estimated angle: %f pi (%.3f%% off)\n', mean(angle_est)/pi,100*(mean(angle_est) - ph)/ph)
+fprintf('Error variance: %f | CRLB: %f\n\n',  phase_var, best_phase(noise_std, A, N))
+
+fprintf('---- After Minimization ----\n');
+fprintf('Estimated omega_0: %.0f (%.3f%% off)\n', omega_mle, 100*(omega_mle - omega_0)/omega_0);
+fprintf('Estimated angle: %f pi (%.3f%% off)\n', phase_mle/pi,100*(phase_mle - ph)/ph)
+
+
+%% Helper functions
 function snr = SNR(A, sigma)
     snr = A^2/(2*sigma^2);
 end
